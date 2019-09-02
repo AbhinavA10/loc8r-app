f9e10673c1bb2e91c6f3ecec22256b1e524532c3
Abhi <abhiraja1012@gmail.com>
2019-09-01 21:49:51 -0400

    add login functionality

--- app_public/src/app/framework/framework.component.ts
+++ app_public/src/app/framework/framework.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service'; // needed so we can login and logout from the base page
+import { User } from '../user';
 
 @Component({
   selector: 'app-framework',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrameworkComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) // inject imported service
+  { }
 
   ngOnInit() {
   }
+  /**
+   * Called when the logout button is clickd
+   * Tells the authentication service to log the user out
+   */
+  public doLogout(): void {
+    this.authenticationService.logout();
+  }
 
+  /**
+   * Wrapper to determine if user is logged in
+   */
+  public isLoggedIn(): boolean {
+    return this.authenticationService.isLoggedIn();
+  }
+
+  /**
+   * Returns the username of the user
+   */
+  public getUsername(): string {
+    const user: User = this.authenticationService.getCurrentUser();
+    return user ? user.name : 'Guest';
+  }
 }
